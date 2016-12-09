be224e2800ba147aa4eb1c27ddcabab54684c40f
Ivan Kosharov <IKosharov@infragistics.com>
2016-12-09 16:18:06 +0200

    making the app preserve credentials in localstorage so that users don't have to login on every refresh

--- js/store.js
+++ js/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
+import { loadState, saveState } from './localStorage';
 import rootReducer from './reducers';
 
 export let store;
@@ -7,10 +8,18 @@ export let store;
 export default function configureStore(browserHistory) {
   const middleware = routerMiddleware(browserHistory);
 
+  var persistedState = loadState();
   store =  createStore(
     rootReducer,
+    persistedState,
     applyMiddleware(middleware)
   );
+
+  store.subscribe(() => {
+    saveState({
+      auth: store.getState().auth
+    });
+  });
 
   return store;
 }
